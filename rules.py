db9ce2ceec7ec25c5cd50a343021e23c033954b4
gaspard1223 <coudercgaspard@gmail.com>
2023-08-30 17:30:51 +0200

    j'ai fini la regle 54 (normalement)

--- rules.py
+++ rules.py
@@ -160,7 +160,8 @@ def ideal_rules(i, grille):
 
 ##Cette fonction, qui n'est pas encore terminée, correspond à la règle de transition qui permet de simuler les glider de l'ECA 54##
 
-#w W c g G#
+
+# w W c g G#
 def rule54(i, grille):
     if i == 0:
         a = None
@@ -179,13 +180,12 @@ def rule54(i, grille):
     elif (a, b, c) == (None, b, None):  # Règle obo
         if isinstance(b, Glider):
             return obo(b)
-        
 
     elif (a, b, c) == (None, None, c):  # Règle ooc
         if isinstance(c, Glider):
             if c.vitesse < 0 and c.phase == c.nb_phases:
                 return copy.deepcopy(c)
-            elif c.symbol==c and c.phase == or c.phase == or c.phase == :
+            elif c.symbol == "c" and c.phase == 11 or c.phase == 16 or c.phase == 21:
                 return Glider(i, -1, 0, 0, 0, "w")
             else:
                 return None
@@ -194,38 +194,24 @@ def rule54(i, grille):
         if isinstance(a, Glider):
             if a.vitesse > 0 and a.phase == a.nb_phases:
                 return copy.deepcopy(a)
+            elif a.symbol == "c" and a.phase == 5 or a.phase == 13 or a.phase == 21:
+                return Glider(i, 1, 0, 0, 0, "W")
             else:
-                return None#############################################################################################################################
-        elif a == 3 or a == 4 or a == 5:
-            return Glider(i, 1, 0, 0, 0, "A")
-        elif a == 6 or a == 7 or a == 9 or a == 10:
-            return None
-        elif a == 8:
-            return Glider(i, 1, 0, 0, 0, "R")
+                return None  #############################################################################################################################
+
     elif (a, b, c) == (a, None, c):  # Règle aoc
         if isinstance(a, Glider) and isinstance(c, Glider):
             if a.vitesse > 0 and c.vitesse < 0:
                 if a.phase == a.nb_phases and c.phase == c.nb_phases:
-                    if a.symbol == "O":
-                        if c.symbol == "y":
+                    if a.symbol == "W":
+                        if c.symbol == "w":
                             return 1
-                        elif c.symbol == "n":
-                            return 2
-                    elif a.symbol == "A":
-                        if c.symbol == "y":
-                            return 3
-                        elif c.symbol == "n":
-                            return 4
-                        elif c.symbol == "L":
-                            return copy.deepcopy(c)
-                    elif a.symbol == "R":
-                        if c.symbol == "L":
-                            return copy.deepcopy(c)
-                        else:
-                            return copy.deepcopy(a)
                 elif a.phase == a.nb_phases:
                     return copy.deepcopy(a)
                 elif c.phase == c.nb_phases:
+                    return copy.deepcopy(c)
+            elif c.vitesse > 0:
+                if a.phase == a.nb_phases:
                     return copy.deepcopy(c)
             elif c.vitesse < 0:
                 if c.phase == c.nb_phases:
@@ -236,24 +222,10 @@ def rule54(i, grille):
                 if isinstance(b, Glider):
                     return obo(b)
             else:
-                if a.phase == a.nb_phases and b.phase != b.nb_phases:
-                    if a.symbol == "O":
-                        if b.symbol == "y":
-                            return 1
-                        elif b.symbol == "n":
+                if a.phase == a.nb_phases:
+                    if a.symbol == "W":
+                        if b.symbol == "w":
                             return 2
-                    elif a.symbol == "A":
-                        if b.symbol == "y":
-                            return 3
-                        elif b.symbol == "n":
-                            return 4
-                        elif b.symbol == "L":
-                            return copy.deepcopy(b)
-                    elif a.symbol == "R":
-                        if b.symbol == "L":
-                            return copy.deepcopy(b)
-                        else:
-                            return copy.deepcopy(a)
                 else:
                     return obo(b)
     elif (a, b, c) == (None, b, c):  # Règle obc
@@ -262,37 +234,4 @@ def rule54(i, grille):
                 if isinstance(b, Glider):
                     return obo(b)
             else:
-                if c.phase == c.nb_phases:
-                    if b.symbol == "O":
-                        if c.symbol == "y":
-                            return 1
-                        elif c.symbol == "n":
-                            return 2
-                    elif b.symbol == "A":
-                        if c.symbol == "y":
-                            return 3
-                        elif c.symbol == "n":
-                            return 4
-                        elif c.symbol == "L":
-                            return copy.deepcopy(c)
-                    elif b.symbol == "R":
-                        if c.symbol == "L":
-                            return copy.deepcopy(c)
-                        else:
-                            return copy.deepcopy(b)
-                    elif b.symbol == "Y":
-                        if c.symbol == "L":
-                            return 5
-                        elif c.symbol == "y":
-                            return 6
-                        elif c.symbol == "n":
-                            return 7
-                    elif b.symbol == "N":
-                        if c.symbol == "L":
-                            return 8
-                        elif c.symbol == "y":
-                            return 9
-                        elif c.symbol == "n":
-                            return 10
-                else:
-                    return obo(b)
+                return None
